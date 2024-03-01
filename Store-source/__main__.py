66f63b4de787dfe8b7d95ac55aebb49ac3745e97
SCOS100Alt <oscar.allegro@icmelissa.edu.it>
2024-03-01 11:16:05 +0100

    modified:   Store-source/__main__.py

--- Store-source/__main__.py
+++ Store-source/__main__.py
@@ -2,7 +2,10 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("SCOS App Store")
+window.geometry("800x400")
 window.resizable(False, False)
+exit = tk.Button(text="Exit", command="exit")
+exit.grid(row=1, column=2)
 
 if __name__ == "__main__":
     window.mainloop()
