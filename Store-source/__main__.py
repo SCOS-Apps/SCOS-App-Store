bf1f57c0c28d8ca0786e3c6c33ca2fd4b6cb4a1a
SCOS100Alt <oscar.allegro@icmelissa.edu.it>
2024-04-18 11:28:25 +0200

    modified:   Store-source/__main__.py 	modified:   store-list.ini

--- Store-source/__main__.py
+++ Store-source/__main__.py
@@ -7,13 +7,15 @@ config = configparser.ConfigParser()
 
 url = "https://raw.githubusercontent.com/SCOS-Apps/SCOS-App-Store/main/store-list.ini"
 
+url2 = "https://raw.githubusercontent.com/SCOS-Apps/SCOS-App-Store/main/"
+
 r = requests.get(url)
 
 content = r.content.decode()
 
 config.read_string(content)
 
-print("This store is managed by: " + config["App-Store"]["store-url"] + ". If the GitHub repo isn't correct, please modify the file 'auth'.")
+print("This store is managed by: " + config["App-Store"]["store-url"] + ". If the GitHub repo isn't correct, please modify your source.")
 
 print("SCOS Store v1.0")
 print("Commands:\n1. Install\n2. Remove\n3. Exit")
@@ -22,11 +24,10 @@ while True:
     print(command)
     if (command == "1"):
         req = input("> Install > App: ")
-        for x in (config["Apps"]["app-list"]):
+        for x in config["Apps"]["app-list"]:
             if config["Apps"]["name-" + x] == req:
                 print("YES")
                 print("Dir: " + config["Apps"]["dir-" + x])
-                file = requests.get("https://raw.githubusercontent.com/SCOS-Apps/SCOS-App-Store/main/App-source/" + config["Apps"]["dir-" + x] + "/__install__.py").content
-                exec(file.decode())
+                exec(requests.get(url2 + "App-source/" + config["Apps"]["dir-" + x] + "/__install__.py").content.decode())
     if (command == "3"):
         exit()
